f8e4f5e2642699cb5a892a121eae704ccdc63d74
Rohit Suratekar <rohitsuratekar@gmail.com>
2018-11-18 16:26:55 +0530

    Working copy of Basic Whastapp Sticker Pack after removing whatsapp validator

---
+++ app/src/main/java/com/secretbiology/stickertest/StickerPackListItemViewHolder.java
@@ -0,0 +1,27 @@
+package com.secretbiology.stickertest;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StickerPackListItemViewHolder extends RecyclerView.ViewHolder {
+
+    View container;
+    TextView titleView;
+    TextView publisherView;
+    TextView filesizeView;
+    ImageView addButton;
+    LinearLayout imageRowView;
+
+    StickerPackListItemViewHolder(final View itemView) {
+        super(itemView);
+        container = itemView;
+        titleView = itemView.findViewById(R.id.sticker_pack_title);
+        publisherView = itemView.findViewById(R.id.sticker_pack_publisher);
+        filesizeView = itemView.findViewById(R.id.sticker_pack_filesize);
+        addButton = itemView.findViewById(R.id.add_button_on_list);
+        imageRowView = itemView.findViewById(R.id.sticker_packs_list_item_image_list);
+    }
+}
---
+++ app/src/main/java/com/secretbiology/stickertest/StickerPreviewAdapter.java
@@ -0,0 +1,76 @@
+package com.secretbiology.stickertest;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+public class StickerPreviewAdapter extends RecyclerView.Adapter<StickerPreviewAdapter.StickerPreviewViewHolder> {
+
+    @NonNull
+    private StickerPack stickerPack;
+
+    private final int cellSize;
+    private int cellLimit;
+    private int cellPadding;
+    private final int errorResource;
+
+    private final LayoutInflater layoutInflater;
+
+    StickerPreviewAdapter(
+            @NonNull final LayoutInflater layoutInflater,
+            final int errorResource,
+            final int cellSize,
+            final int cellPadding,
+            @NonNull final StickerPack stickerPack) {
+        this.cellSize = cellSize;
+        this.cellPadding = cellPadding;
+        this.cellLimit = 0;
+        this.layoutInflater = layoutInflater;
+        this.errorResource = errorResource;
+        this.stickerPack = stickerPack;
+    }
+
+    @NonNull
+    @Override
+    public StickerPreviewViewHolder onCreateViewHolder(@NonNull final ViewGroup viewGroup, final int i) {
+        View itemView = layoutInflater.inflate(R.layout.sticker_image, viewGroup, false);
+        StickerPreviewViewHolder vh = new StickerPreviewViewHolder(itemView);
+
+        ViewGroup.LayoutParams layoutParams = vh.stickerPreviewView.getLayoutParams();
+        layoutParams.height = cellSize;
+        layoutParams.width = cellSize;
+        vh.stickerPreviewView.setLayoutParams(layoutParams);
+        vh.stickerPreviewView.setPadding(cellPadding, cellPadding, cellPadding, cellPadding);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final StickerPreviewViewHolder stickerPreviewViewHolder, final int i) {
+        stickerPreviewViewHolder.stickerPreviewView.setImageResource(errorResource);
+        stickerPreviewViewHolder.stickerPreviewView.setImageURI(StickerPackLoader.getStickerAssetUri(stickerPack.identifier, stickerPack.getStickers().get(i).imageFileName));
+    }
+
+    @Override
+    public int getItemCount() {
+        int numberOfPreviewImagesInPack;
+        numberOfPreviewImagesInPack = stickerPack.getStickers().size();
+        if (cellLimit > 0) {
+            return Math.min(numberOfPreviewImagesInPack, cellLimit);
+        }
+        return numberOfPreviewImagesInPack;
+    }
+
+    class StickerPreviewViewHolder extends RecyclerView.ViewHolder {
+
+        public SimpleDraweeView stickerPreviewView;
+
+        StickerPreviewViewHolder(final View itemView) {
+            super(itemView);
+            stickerPreviewView = itemView.findViewById(R.id.sticker_preview);
+        }
+    }
+}
